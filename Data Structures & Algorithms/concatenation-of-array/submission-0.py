6ace48865d66935eb10067393d1d9f5cf5375364
Barathraj M S <msbarathraj.work@gmail.com>
2026-04-18 17:36:12 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        anslist = []
+        anslist = nums
+        anslist = anslist + nums
+        return anslist
