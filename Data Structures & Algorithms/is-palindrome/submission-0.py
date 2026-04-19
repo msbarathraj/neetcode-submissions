0bb2135710975a0e4153c0c9ce12a18387c50f68
Barathraj M S <msbarathraj.work@gmail.com>
2026-04-19 21:49:22 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        news = ''
+        for c in s:
+            if c.isalnum():
+                news += c.lower()
+            
+        return news == news[::-1]
